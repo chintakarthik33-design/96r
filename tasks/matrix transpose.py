1b9f464260037199e423f339eb90873cccb154fc
chintakarthik33-design <chintakarthik33@gmail.com>
2026-06-21 23:23:46 +0530

    Update matrix transpose.py

--- tasks/matrix transpose.py
+++ tasks/matrix transpose.py
@@ -1,8 +1,9 @@
 l1=[[1,2,3],[4,5,6]]
+print("Original Matrix:", l1) # Ee line add chey
 transpose=[]
 for i in range(len(l1[0])):
     row=[]
     for j in range(len(l1)):
         row.append(l1[j][i])
     transpose.append(row)
-print(transpose)
+print("Transposed Matrix:", transpose)
